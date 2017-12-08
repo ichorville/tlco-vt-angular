3ea26b6f9eb927d4156a611ae3b3a9d2366dd2c5
Manoj Jayalath <manojj@axienta.lk>
2017-12-08 13:24:56 +0530

    validateUser method temporarily added

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -3,15 +3,29 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from
 
 @Injectable()
 export class AuthService implements CanActivate {
-  public authToken;
-  private isAuthenticated = true; // Set this value dynamically
-  
-  constructor(private router: Router) { }
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (this.isAuthenticated) {
-      return true
-    }
-    this.router.navigate(['/sessions/signin']);
-    return false;
-  }
+	public authToken;
+	private isAuthenticated = false; // Set this value dynamically
+
+	constructor (
+		private router: Router
+	) { }
+
+	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+		if (this.isAuthenticated) {
+			return true;
+		}
+		this.router.navigate(['/sessions/signin']);
+		return false;
+	}
+
+	validateUser(formData: any): Promise<boolean> {
+		return Promise.resolve(true).then(() => {
+			if (formData.username == 'LeslyK' && formData.password == '123456') {
+				this.isAuthenticated = true;
+				return true;
+			} else {
+				return false;
+			}
+		});
+	}
 }
