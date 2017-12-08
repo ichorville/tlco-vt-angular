b09243482ea904fbcece4d7e1fb917660fb110f4
Manoj Jayalath <manoj.jayalath@axienta.lk>
2017-12-08 13:26:39 +0530

    File indented to follow tab-size-4

--- src/app/views/sessions/sessions.module.ts
+++ src/app/views/sessions/sessions.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from "@angular/router";
-import { 
-  MatProgressBarModule,
-  MatButtonModule,
-  MatInputModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatIconModule
- } from '@angular/material';
+import {
+	MatProgressBarModule,
+	MatButtonModule,
+	MatInputModule,
+	MatCardModule,
+	MatCheckboxModule,
+	MatIconModule
+} from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { CommonDirectivesModule } from '../../directives/common/common-directives.module';
@@ -22,19 +22,26 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { ErrorComponent } from './error/error.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatProgressBarModule,
-    MatButtonModule,
-    MatInputModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatIconModule,
-    FlexLayoutModule,
-    RouterModule.forChild(SessionsRoutes)
-  ],
-  declarations: [ForgotPasswordComponent, LockscreenComponent, SigninComponent, SignupComponent, NotFoundComponent, ErrorComponent]
+	imports: [
+		CommonModule,
+		FormsModule,
+		ReactiveFormsModule,
+		MatProgressBarModule,
+		MatButtonModule,
+		MatInputModule,
+		MatCardModule,
+		MatCheckboxModule,
+		MatIconModule,
+		FlexLayoutModule,
+		RouterModule.forChild(SessionsRoutes)
+	],
+	declarations: [
+		ForgotPasswordComponent, 
+		LockscreenComponent, 
+		SigninComponent, 
+		SignupComponent, 
+		NotFoundComponent, 
+		ErrorComponent
+	]
 })
 export class SessionsModule { }
