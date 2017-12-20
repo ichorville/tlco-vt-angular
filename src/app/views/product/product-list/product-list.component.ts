5169edea48377cfe442158ef839194ff08ccaf46
Manoj Jayalath <manoj.jayalath@axienta.lk>
2017-12-20 16:31:48 +0530

    Parameters for Datatable module added

--- src/app/views/product/product-list/product-list.component.ts
+++ src/app/views/product/product-list/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductService } from '../product.service';
+
 @Component({
 	selector: 'app-product-list',
 	templateUrl: './product-list.component.html',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-	constructor() { }
+	rows: any;
+
+	constructor(
+		private _ps: ProductService
+	) { }
 
 	ngOnInit() {
+		this.rows = this._ps.messages;
 	}
-
 }
