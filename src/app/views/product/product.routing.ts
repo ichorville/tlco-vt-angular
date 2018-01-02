80ac1d398b4dfd60b4f4fa3c12c36d7c0e635b80
Manoj Jayalath <manoj.jayalath@axienta.lk>
2018-01-02 11:00:42 +0530

    Product add and edit routes configured

--- src/app/views/product/product.routing.ts
+++ src/app/views/product/product.routing.ts
@@ -1,22 +1,30 @@
 import { Routes } from '@angular/router';
 
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+
 import { AdminLayoutComponent } from '../../components/common/layouts/admin-layout/admin-layout.component';
 
 export const ProductRoutes: Routes = [
     {
-        path: '',
-        children: [
-            {
-                path: 'list',
-                component: ProductListComponent,
-                data: { title: 'List', breadcrumb: 'List' },
-            }, 
-            {
-                path: 'add',
-                component: ProductListComponent,
-                data: { title: 'Add', breadcrumb: 'Add' },
-            }
-        ]
+		path: '',
+		redirectTo: '/products/list',
+		pathMatch: 'full'
+	},
+    {
+        path: 'list',
+        component: ProductListComponent,
+        data: { title: 'List', breadcrumb: 'List' },
+    },
+    {
+        path: 'config',
+        component: ProductAddComponent,
+        data: { title: 'Configuration', breadcrumb: 'Configuration' },
+    },
+    {
+        path: ':id/config',
+        component: ProductEditComponent,
+        data: { title: 'Configuration', breadcrumb: 'Configuration' },
     }
 ];
