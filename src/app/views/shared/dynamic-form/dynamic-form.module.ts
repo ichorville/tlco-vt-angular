5a9c8bc9f509ee9ae1de3e0ace7215139e365cf4
Manoj Jayalath <manoj.jayalath@axienta.lk>
2018-01-02 16:12:13 +0530

    Generated dynamic-form module

---
+++ src/app/views/shared/dynamic-form/dynamic-form.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DynamicFormComponent } from './dynamic-form.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DynamicFormComponent]
+})
+export class DynamicFormModule { }
