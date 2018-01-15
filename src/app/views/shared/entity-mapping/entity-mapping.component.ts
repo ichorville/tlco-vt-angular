5979e4b4189900245a4b706edff295067eb7595d
Manoj Jayalath <manoj.jayalath@axienta.lk>
2018-01-15 12:48:59 +0530

    Generated entity-mapping component

---
+++ src/app/views/shared/entity-mapping/entity-mapping.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entity-mapping',
+  templateUrl: './entity-mapping.component.html',
+  styleUrls: ['./entity-mapping.component.css']
+})
+export class EntityMappingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
