a8aa1926573220abc0648233b019f581ac99ce3d
Manoj Jayalath <manoj.jayalath@axienta.lk>
2018-01-15 12:48:25 +0530

    Generated entity-mapping module

---
+++ src/app/views/shared/entity-mapping/entity-mapping.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EntityMappingComponent } from './entity-mapping.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [EntityMappingComponent]
+})
+export class EntityMappingModule { }
