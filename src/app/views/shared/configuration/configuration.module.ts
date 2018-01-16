19f55ac8064af46797133af13b4a8ddea40d17bb
Manoj Jayalath <manoj.jayalath@axienta.lk>
2018-01-16 10:55:35 +0530

    Entity-mapping module added

--- src/app/views/shared/configuration/configuration.module.ts
+++ src/app/views/shared/configuration/configuration.module.ts
@@ -18,6 +18,7 @@ import {
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { QuillModule } from 'ngx-quill';
 import { DynamicStepFormModule } from '../dynamic-step-form/dynamic-step-form.module';
+import { EntityMappingModule } from '../entity-mapping/entity-mapping.module';
 
 import { ConfigurationComponent } from './configuration.component';
 import { ConfigurationService } from './configuration.service';
@@ -41,7 +42,8 @@ import { ConfigurationService } from './configuration.service';
 		MatStepperModule,
 		FlexLayoutModule,
 		QuillModule,
-		DynamicStepFormModule
+		DynamicStepFormModule,
+		EntityMappingModule
 	],
 	declarations: [
 		ConfigurationComponent
