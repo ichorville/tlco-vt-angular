abc2ac1cabd197408d92006c4dbe1315a0f17d27
Manoj Jayalath <manoj.jayalath@axienta.lk>
2018-01-16 11:06:21 +0530

    Dummmy step data added

--- src/app/views/shared/entity-mapping/entity-mapping.component.ts
+++ src/app/views/shared/entity-mapping/entity-mapping.component.ts
@@ -1,15 +1,81 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-entity-mapping',
-  templateUrl: './entity-mapping.component.html',
-  styleUrls: ['./entity-mapping.component.css']
+	selector: 'app-entity-mapping',
+	templateUrl: './entity-mapping.component.html',
+	styleUrls: ['./entity-mapping.component.css']
 })
 export class EntityMappingComponent implements OnInit {
 
-  constructor() { }
+	tasks: any;
 
-  ngOnInit() {
-  }
+	constructor() {
+		this.tasks = [
+			{
+				text: 'Lorem, ipsum dolor sit amet',
+				status: 0
+			}, 
+			{
+				text: 'Lorem, ipsum dolor sit amet',
+				status: 0
+			}, 
+			{
+				text: 'Lorem, ipsum dolor sit amet',
+				status: 1
+			}, 
+			{
+				text: 'Lorem, ipsum dolor sit amet',
+				status: 1
+			}, 
+			{
+				text: 'Lorem, ipsum dolor sit amet',
+				status: 1
+			},
+			{
+				text: 'Lorem, ipsum dolor sit amet',
+				status: 0
+			}, 
+			{
+				text: 'Lorem, ipsum dolor sit amet',
+				status: 0
+			}, 
+			{
+				text: 'Lorem, ipsum dolor sit amet',
+				status: 1
+			}, 
+			{
+				text: 'Lorem, ipsum dolor sit amet',
+				status: 1
+			}, 
+			{
+				text: 'Lorem, ipsum dolor sit amet',
+				status: 1
+			},
+			{
+				text: 'Lorem, ipsum dolor sit amet',
+				status: 0
+			}, 
+			{
+				text: 'Lorem, ipsum dolor sit amet',
+				status: 0
+			}, 
+			{
+				text: 'Lorem, ipsum dolor sit amet',
+				status: 1
+			}, 
+			{
+				text: 'Lorem, ipsum dolor sit amet',
+				status: 1
+			}, 
+			{
+				text: 'Lorem, ipsum dolor sit amet',
+				status: 1
+			}
+		];
+	 }
+
+	ngOnInit() {
+		
+	}
 
 }
