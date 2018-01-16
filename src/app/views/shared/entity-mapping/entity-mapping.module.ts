c1bba769a023f5dd0c53fcfb68a13c355a2fac2e
Manoj Jayalath <manoj.jayalath@axienta.lk>
2018-01-16 11:05:15 +0530

    Material imports changed to follow imports in full project version

--- src/app/views/shared/entity-mapping/entity-mapping.module.ts
+++ src/app/views/shared/entity-mapping/entity-mapping.module.ts
@@ -1,11 +1,54 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+	MatListModule,
+	MatIconModule,
+	MatButtonModule,
+	MatCardModule,
+	MatMenuModule,
+	MatSlideToggleModule,
+	MatGridListModule,
+	MatChipsModule,
+	MatCheckboxModule,
+	MatRadioModule,
+	MatTabsModule,
+	MatInputModule,
+	MatProgressBarModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { QuillModule } from 'ngx-quill';
+import { SearchModule } from '../search/search.module';
+
 import { EntityMappingComponent } from './entity-mapping.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [EntityMappingComponent]
+	imports: [
+		CommonModule,
+		FormsModule, 
+		ReactiveFormsModule,
+		MatListModule,
+		MatIconModule,
+		MatButtonModule,
+		MatCardModule,
+		MatMenuModule,
+		MatSlideToggleModule,
+		MatGridListModule,
+		MatChipsModule,
+		MatCheckboxModule,
+		MatRadioModule,
+		MatTabsModule,
+		MatInputModule,
+		MatProgressBarModule,
+		FlexLayoutModule,
+		QuillModule,
+		SearchModule
+	],
+	declarations: [
+		EntityMappingComponent
+	],
+	exports: [
+		EntityMappingComponent
+	]
 })
 export class EntityMappingModule { }
