fe4b9324d69cb93093322265d634ddc3f2cd6ed4
Manoj Jayalath <manoj.jayalath@axienta.lk>
2018-01-16 17:14:56 +0530

    Redraw list on search functionality implemented

--- src/app/views/shared/entity-mapping/entity-mapping.component.ts
+++ src/app/views/shared/entity-mapping/entity-mapping.component.ts
@@ -8,74 +8,132 @@ import { Component, OnInit } from '@angular/core';
 export class EntityMappingComponent implements OnInit {
 
 	tasks: any;
+	isValid = true;
+	selectToggleFlag = false;
+	selectedTotal: number;
+
+	tempArr: any[];
 
 	constructor() {
+		this.tempArr = [];
 		this.tasks = [
 			{
-				text: 'Lorem, ipsum dolor sit amet',
-				status: 0
+				text: 'Cranjus McBasketball',
+				status: false
 			}, 
 			{
-				text: 'Lorem, ipsum dolor sit amet',
-				status: 0
+				text: 'Minty Cherubandtug',
+				status: false
 			}, 
 			{
-				text: 'Lorem, ipsum dolor sit amet',
-				status: 1
+				text: 'Mary Beth BethBeth',
+				status: true
 			}, 
 			{
-				text: 'Lorem, ipsum dolor sit amet',
-				status: 1
+				text: 'Helena Bottom-Farter',
+				status: true
 			}, 
 			{
-				text: 'Lorem, ipsum dolor sit amet',
-				status: 1
+				text: 'Beefy McWhatnow',
+				status: true
 			},
 			{
-				text: 'Lorem, ipsum dolor sit amet',
-				status: 0
+				text: 'Tammy BundleBalls',
+				status: false
 			}, 
 			{
-				text: 'Lorem, ipsum dolor sit amet',
-				status: 0
+				text: 'Captain Melvin Seashorse',
+				status: false
 			}, 
 			{
-				text: 'Lorem, ipsum dolor sit amet',
-				status: 1
+				text: 'Dr. Shrimp Puerto Rico',
+				status: true
 			}, 
 			{
-				text: 'Lorem, ipsum dolor sit amet',
-				status: 1
+				text: 'Simmy Cantstandyourbitz',
+				status: true
 			}, 
 			{
-				text: 'Lorem, ipsum dolor sit amet',
-				status: 1
+				text: 'Wandamian Crucifixplate',
+				status: true
 			},
 			{
-				text: 'Lorem, ipsum dolor sit amet',
-				status: 0
+				text: 'Denise Fat',
+				status: false
 			}, 
 			{
-				text: 'Lorem, ipsum dolor sit amet',
-				status: 0
+				text: 'Jury Prosciutto',
+				status: false
 			}, 
 			{
-				text: 'Lorem, ipsum dolor sit amet',
-				status: 1
+				text: 'Rickyticky Bobbywobbin',
+				status: true
 			}, 
 			{
-				text: 'Lorem, ipsum dolor sit amet',
-				status: 1
+				text: 'Dungaresse Weatherspoons',
+				status: true
 			}, 
 			{
-				text: 'Lorem, ipsum dolor sit amet',
-				status: 1
+				text: 'Eyna Mouthhole',
+				status: true
 			}
 		];
 	 }
 
 	ngOnInit() {
-		
+		this.tempArr = this.tasks;
+		this.calculateTotalSelected();	
+	}
+
+	toggleAll() {
+		// console.log(value);
+		console.log(this.selectToggleFlag);
+		this.tasks.forEach(task => task.status = this.selectToggleFlag);
+		// if (this.selectToggleFlag == true) {
+		// 	this.tasks.forEach(task => task.status = true);
+		// } else if (this.selectToggleFlag == false) {
+		// 	this.tasks.forEach(task => task.status = false);
+		// }
+		this.calculateTotalSelected();
+	}
+
+	toggleEntity() {
+		if (this.selectToggleFlag == true) {
+			this.selectToggleFlag = !this.selectToggleFlag;
+		}
+		this.calculateTotalSelected();	
+		return;
+	}
+
+	calculateTotalSelected() {
+		this.selectedTotal = 0;
+		this.tempArr.forEach(task => task.status == true ? this.selectedTotal++ : this.selectedTotal);
+		console.log(this.selectedTotal);
+	}
+
+	reloadList(event) {
+		console.log(event);
+		if (event != undefined) {
+			if (event == 'NDF') {
+				this.isValid = false;
+			} else {
+				this.isValid = true;
+				this.tasks = event;
+
+				// // calculate the no of pagination pages
+				// this._ds.getPageCount(this.rows.length).then((pages) => {
+				// 	this.pages = pages;
+				// });
+				// // paginate the whole dataset according to the pagination pages
+				// this._ds.paginate(10, this.rows).then((filteredRows) => {
+				// 	this.totalPaginatedRows = filteredRows;
+				// 	this.filteredRows = filteredRows[0].items;
+				// 	this.isInit = true;
+				// });
+				// // load the first data set hence first selected page
+				// this.selectedPage = 1;
+			}
+		}
 	}
 
 }
