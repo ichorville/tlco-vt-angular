8827aefcfc9e406510a1bb36cbf0f3bfb206bd49
Manoj Jayalath <manoj.jayalath@axienta.lk>
2018-01-22 16:54:42 +0530

    New Todo file added with remaining tasks periof

---
+++ src/app/todo.ts
@@ -0,0 +1,19 @@
+/**
+ * This file contains the remaining todos in the application
+ * 
+ * // Configuration module
+ * 1. form-steps binding with checkbox
+ * 2. data-step binding from one step to another: as in if a step was complete and if the previous step gets incomplete again
+ *     the system should block the option to proceed forward where the incomplete step has to be completed first.
+ * 3. display details from the first form object in the second page, even with rea time editing
+ * 
+ * // Datatable module
+ * 1. column sorting
+ * 
+ * // Dynamic Step form module
+ * 1. allign steps to be in multiple rows rather than be in one single row when the the entity configuration changes
+ * 2. form validation logic on code behind eg: onValueChnged()
+ * 
+ * // Search module 
+ * 1. multiple search process in Entity-mapping module have to be simplified for each search component used
+ */
