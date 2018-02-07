ee5875214e24251c5922b8c2bf94f5621143a884
Jay <mjzeel@gmail.com>
2018-02-07 21:04:37 +0530

    Distributor-list component updated

--- src/app/views/distributor/distributor-list/distributor-list.component.ts
+++ src/app/views/distributor/distributor-list/distributor-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DistributorService } from '../distributor.service';
+
 @Component({
-  selector: 'app-distributor-list',
-  templateUrl: './distributor-list.component.html',
-  styleUrls: ['./distributor-list.component.css']
+	selector: 'app-distributor-list',
+	templateUrl: './distributor-list.component.html',
+	styleUrls: ['./distributor-list.component.css']
 })
 export class DistributorListComponent implements OnInit {
 
-  constructor() { }
+	rows: any;
+	url: string;
 
-  ngOnInit() {
-  }
+	constructor(
+		private _ds: DistributorService
+	) { 
+		this.url = 'distributors';
+	}
 
+	ngOnInit() {
+		this.rows = this._ds.messages;
+	}
 }
