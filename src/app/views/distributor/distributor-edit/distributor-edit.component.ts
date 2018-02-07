92e2cf5862278e098afebd9881676f31f6ac1391
Jay <mjzeel@gmail.com>
2018-02-07 19:29:05 +0530

    Generated distributor-edit component

---
+++ src/app/views/distributor/distributor-edit/distributor-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-distributor-edit',
+  templateUrl: './distributor-edit.component.html',
+  styleUrls: ['./distributor-edit.component.css']
+})
+export class DistributorEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
