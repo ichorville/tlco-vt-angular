f440a3628aa3d18a8ffdc986ac6d7570b259c380
Jay <mjzeel@gmail.com>
2018-02-07 19:28:42 +0530

    Generated distributor-add component

---
+++ src/app/views/distributor/distributor-add/distributor-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-distributor-add',
+  templateUrl: './distributor-add.component.html',
+  styleUrls: ['./distributor-add.component.css']
+})
+export class DistributorAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
