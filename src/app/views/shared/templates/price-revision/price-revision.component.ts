2b5485886bb4cf7e7b5d094acf2ca34056db2d28
Jay <mjzeel@gmail.com>
2018-02-07 19:32:31 +0530

    File indented to tab-size 4

--- src/app/views/shared/templates/price-revision/price-revision.component.ts
+++ src/app/views/shared/templates/price-revision/price-revision.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-price-revision',
-  templateUrl: './price-revision.component.html',
-  styleUrls: ['./price-revision.component.css']
+	selector: 'app-price-revision',
+	templateUrl: './price-revision.component.html',
+	styleUrls: ['./price-revision.component.css']
 })
 export class PriceRevisionComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		
+	}
 
 }
