5ced43734492652dfafc1cae6e74e21dfe69c44d
Jay <mjzeel@gmail.com>
2018-02-07 19:26:54 +0530

    Generated distributor module

--- src/app/views/distributor/distributor.module.ts
+++ src/app/views/distributor/distributor.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DistributorComponent } from './distributor.component';
+import { DistributorAddComponent } from './distributor-add/distributor-add.component';
+import { DistributorEditComponent } from './distributor-edit/distributor-edit.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [DistributorComponent]
+	imports: [
+		CommonModule
+	],
+	declarations: [
+		DistributorComponent,
+		DistributorAddComponent,
+		DistributorEditComponent
+	],
+	exports: [
+		DistributorComponent
+	]
 })
 export class DistributorModule { }
