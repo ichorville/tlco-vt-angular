a8a5501648d69c90f4f200994860efeb83dc2c07
Jay <mjzeel@gmail.com>
2018-02-07 19:28:18 +0530

    Generated distributor-list component

---
+++ src/app/views/distributor/distributor-list/distributor-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-distributor-list',
+  templateUrl: './distributor-list.component.html',
+  styleUrls: ['./distributor-list.component.css']
+})
+export class DistributorListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
