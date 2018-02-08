eab7f1740c3531405ce470110d73b6029d970e7c
Jay <mjzeel@gmail.com>
2018-02-08 11:32:33 +0530

    Generated simple-datatable component

---
+++ src/app/views/shared/simple-datatable/simple-datatable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-datatable',
+  templateUrl: './simple-datatable.component.html',
+  styleUrls: ['./simple-datatable.component.css']
+})
+export class SimpleDatatableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
