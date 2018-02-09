efd5909a82b41da4472214aae045757da3668eb4
Jay <mjzeel@gmail.com>
2018-02-09 15:20:16 +0530

    Simple datatable service added

--- src/app/views/shared/simple-datatable/simple-datatable.module.ts
+++ src/app/views/shared/simple-datatable/simple-datatable.module.ts
@@ -1,14 +1,55 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SimpleDatatableComponent } from './simple-datatable.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { 
+	MatIconModule,
+	MatButtonModule,
+	MatToolbarModule,
+	MatSidenavModule,
+	MatMenuModule,
+	MatInputModule,
+	MatDialogModule,
+	MatListModule,
+	MatCheckboxModule,
+	MatTooltipModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { QuillModule } from 'ngx-quill';
+import { CommonDirectivesModule } from '../../../directives/common/common-directives.module';
+import { PaginationModule } from '../pagination/pagination.module';
+import { SearchModule } from '../search/search.module';
 
+import { SimpleDatatableComponent } from './simple-datatable.component';
+import { SimpleDatatableService } from './simple-datatable.service';
 @NgModule({
 	imports: [
-		CommonModule
+		CommonModule,
+		RouterModule,
+		FormsModule, 
+		ReactiveFormsModule,
+		MatIconModule,
+		MatButtonModule,
+		MatToolbarModule,
+		MatSidenavModule,
+		MatMenuModule,
+		MatInputModule,
+		MatDialogModule,
+		MatListModule,
+		MatCheckboxModule,
+		MatTooltipModule,
+		FlexLayoutModule,
+		QuillModule,
+		CommonDirectivesModule,
+		PaginationModule,
+		SearchModule
 	],
 	declarations: [
 		SimpleDatatableComponent
 	], 
+	providers: [
+		SimpleDatatableService
+	],
 	exports: [
 		SimpleDatatableComponent
 	]
