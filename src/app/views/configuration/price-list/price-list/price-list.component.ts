8c33f9cbb493177967f9fbf2e9ccbd69ba67be4a
Jay <mjzeel@gmail.com>
2018-02-09 15:25:13 +0530

    Price list component updated with simple-datatable

--- src/app/views/configuration/price-list/price-list/price-list.component.ts
+++ src/app/views/configuration/price-list/price-list/price-list.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PriceListService } from '../price-list.service';
+
 @Component({
-  selector: 'app-price-list',
-  templateUrl: './price-list.component.html',
-  styleUrls: ['./price-list.component.css']
+	selector: 'app-price-list',
+	templateUrl: './price-list.component.html',
+	styleUrls: ['./price-list.component.css']
 })
 export class PriceListComponent implements OnInit {
 
-  constructor() { }
+	columns: any[];
+	rows: any[];
 
-  ngOnInit() {
-  }
+	priceLists: any[];
 
+	constructor (
+		private _pls: PriceListService
+	) {
+		this.priceLists = [];
+		this.columns = [
+			{ name: 'Price List Type', attr: 'type', type: 'string' },
+			{ name: 'Price List ID', attr: 'id', type: 'string' },
+			{ name: 'Creation Date', attr: 'creationDate', type: 'string' },
+			{ name: 'Expiry Date', attr: 'expiryDate', type: 'string' }
+		];
+	}
+
+	ngOnInit() {
+		this.priceLists = this._pls.priceLists;
+		this.updateRows();
+	}
+
+	private updateRows() {
+		this.rows = [];
+		this.priceLists.forEach(element => {
+			this.rows.push({
+				type: element['type'],
+				id: element['id'],
+				creationDate: element['creationDate'],
+				expiryDate: element['expiryDate']
+			});
+		});
+	}
 }
