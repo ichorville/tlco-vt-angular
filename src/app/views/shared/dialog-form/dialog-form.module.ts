fd7432799649be8492b91331590a26595b57f845
Jay <mjzeel@gmail.com>
2018-02-11 13:06:36 +0530

    Generated Dialog Form

---
+++ src/app/views/shared/dialog-form/dialog-form.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DialogFormComponent } from './dialog-form.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DialogFormComponent]
+})
+export class DialogFormModule { }
