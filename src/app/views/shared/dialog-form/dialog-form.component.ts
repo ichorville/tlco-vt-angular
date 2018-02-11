f11154530ec2bc490bd37687c8b3f2d3673ad921
Jay <mjzeel@gmail.com>
2018-02-11 13:06:59 +0530

    Generated dialog form component

---
+++ src/app/views/shared/dialog-form/dialog-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-form',
+  templateUrl: './dialog-form.component.html',
+  styleUrls: ['./dialog-form.component.css']
+})
+export class DialogFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
