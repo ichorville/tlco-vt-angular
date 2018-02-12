1acb33eab4a13b08e3aadcd0920c6085720e6a67
Jay <mjzeel@gmail.com>
2018-02-12 19:39:35 +0530

    Input parameters added to component

--- src/app/views/shared/templates/price-revision/price-revision.component.ts
+++ src/app/views/shared/templates/price-revision/price-revision.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
 	selector: 'app-price-revision',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./price-revision.component.css']
 })
 export class PriceRevisionComponent implements OnInit {
+
+	@Input()
+	priceListItems: any[];
 
 	constructor() { }
 
