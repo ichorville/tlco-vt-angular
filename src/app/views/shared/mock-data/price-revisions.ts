7edd94c5ce42d50f17a5ee5f7df4c11f74d431b1
Manoj Jayalath <manoj.jayalath@axienta.lk>
2018-02-15 17:14:44 +0530

    New mock data class and dummy data added

---
+++ src/app/views/shared/mock-data/price-revisions.ts
@@ -0,0 +1,26 @@
+export var PRICE_REVISIONS = [
+    {
+        type: 'DPL',
+        id: '123123',
+        price: '2,300',
+        effectiveDate: '01/02/2018 02:57 AM',
+        expiryDate: '02/02/2018 11:48 AM',
+        activated: false
+    },
+    {
+        type: 'DPL',
+        id: '123123',
+        price: '600',
+        effectiveDate: '03/02/2018 09:34 AM',
+        expiryDate: '04/02/2018 02:56 AM',
+        activated: false
+    },
+    {
+        type: 'DPL',
+        id: '123123',
+        price: '200',
+        effectiveDate: '05/02/2018 03:06 AM',
+        expiryDate: '06/02/2018 05:38 AM',
+        activated: true
+    }
+];
